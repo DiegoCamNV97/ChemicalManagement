6931160458c4509e5d0a209e38805e9cc3746f26
Diego Camilo Noguera Vasquez <diegoc.noguerav@outlook.com>
2024-09-11 19:18:54 -0500

    Creación BackEnd

---
+++ BackEnd/dataBase/src/main/java/com/chemicalmanagement/dcnv/dataBase/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.chemicalmanagement.dcnv.dataBase;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataBaseApplication.class);
+	}
+
+}
